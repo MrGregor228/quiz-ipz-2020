35ebd6ed9cec81f3c0cbdb1b7ac60224b84a9cfc
MrGregor228 <39625405+MrGregor228@users.noreply.github.com>
2020-11-06 16:05:16 +0200

    The test

---
+++ js/js.js
@@ -0,0 +1,112 @@
+// Переключение вариантов ответа
+let labels = document.querySelectorAll(".radio-group label"),
+    next_question_btn = document.getElementById('nxt_quest'),
+    radio_group = document.querySelector(".radio-group"),
+    choosed_variant = "",
+    radios = document.querySelectorAll(".radio-group label input[type=radio]");
+radios.forEach((item) => {
+    item.addEventListener("click", (e) => {
+        let target = e.target;
+        for (i = 0; i < labels.length; i++) {
+            labels[i].classList.remove("active");
+        }
+        target.closest("label").classList.add("active");
+
+        choosed_variant = item.value;
+        if (item.checked) {
+            next_question_btn.removeAttribute('disabled', 'disabled');
+        } else {
+            choosed_variant = "";
+            next_question_btn.setAttribute('disabled', 'disabled');
+        }
+    });
+});
+// Переключение вариантов ответа
+
+
+// Получение данных из JSON с вопросами
+const getData = async function (url) {
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error(`Ошибка по адресу ${url}, статус ошибки ${response.status}!`);
+    }
+    return await response.json();
+};
+const quiz_url = "https://raw.githubusercontent.com/MrGregor228/different-jsons/master/questions.json";
+
+let total_amount = 0,
+    correct_answer_amount = 0,
+    total_amount_span = document.querySelector('.total__amount'),
+    correct_answer_span = document.querySelector('.correct__answer'),
+
+    question_number = document.querySelector('.question-number'),
+    question = document.querySelector('.question'),
+
+    answ = document.querySelectorAll('.radio-group label span'),   
+
+    result_container = document.querySelector('.result'),
+    result_span = document.querySelector('.result span'),
+
+    num = 0;
+
+function cleanFields() {
+    result_container.style.display = "flex";
+    result_span.textContent = `${correct_answer_amount} / ${total_amount}`;
+    radio_group.remove();
+    question.remove();
+    question_number.remove();
+    next_question_btn.remove();
+    document.querySelector('.next-question').remove();
+}    
+
+getData(quiz_url).then((data) => {
+    for (let i = 0; i < data.length; i++) {
+        total_amount = total_amount + +data[i].amount;
+    }
+    total_amount_span.textContent = total_amount;
+    correct_answer_span.textContent = correct_answer_amount;
+    answ.forEach((item, i) => {
+        item.textContent = data[0].answers[i];
+    });
+    question_number.textContent = data[0].number;
+    question.textContent = data[0].question;
+    // radios.forEach((item)=>{
+    //     item.addEventListener('click', ()=>{
+    //         console.log(choosed_variant + " = ",data[num].right == choosed_variant);
+    //         console.log(num);
+    //         console.log(data[num]);
+    //     });
+    // });
+    next_question_btn.addEventListener('click', () => { 
+        if (num <= data.length) {
+            if(data[num].right == choosed_variant) {
+                correct_answer_amount = correct_answer_amount + +data[num].amount
+                correct_answer_span.textContent = correct_answer_amount;
+            } 
+        } else {
+            cleanFields();
+        } 
+        num++;
+        if (num <= data.length - 1) {
+            answ.forEach((item, i) => {
+                item.textContent = data[num].answers[i];
+            });
+            question_number.textContent = data[num].number;
+            question.textContent = data[num].question;
+            for (i = 0; i < labels.length; i++) {
+                labels[i].classList.remove("active");
+                radios[i].checked = false;
+            }           
+            
+            choosed_variant = "";
+            next_question_btn.setAttribute('disabled', 'disabled');
+            if (num == data.length - 1) {
+                next_question_btn.textContent = "Завершити";
+            }
+
+            
+        } else {
+            cleanFields();
+        }        
+    });
+});
