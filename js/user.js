f03dfce123a26e3a9e821b3cede4ba82af08330c
MrGregor228 <39625405+MrGregor228@users.noreply.github.com>
2020-11-22 16:51:21 +0200

    gyk

--- js/user.js
+++ js/user.js
@@ -112,26 +112,7 @@ question_btn.addEventListener('click', () => {
 
 
 // ---------------------------------- FireBase ----------------------------------
-let firebaseConfig = {
-    apiKey: "AIzaSyDZdW65kJyGc1LRz5J-pZai0XtbdUB85bs",
-    authDomain: "ipz-2020-test-database-84e09.firebaseapp.com",
-    databaseURL: "https://ipz-2020-test-database-84e09.firebaseio.com",
-    projectId: "ipz-2020-test-database-84e09",
-    storageBucket: "ipz-2020-test-database-84e09.appspot.com",
-    messagingSenderId: "168977355305",
-    appId: "1:168977355305:web:6cd8931566416e45854238"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-function writeData() {
-    if (!localStorage.getItem('User_Name') || !localStorage.getItem('User_Group')) {
-        console.log("LocalStorage User_Name or User_Group is not defined");
-    } else {
-        firebase.database().ref("User").set({
-            name: localStorage.getItem('User_Name'),
-            group: localStorage.getItem('User_Group')
-        });
-    }
-}
-writeData();
+firebase.database().ref("User").set({
+    name: localStorage.getItem("User_Name"),
+    group: localStorage.getItem("User_Group")
+});
